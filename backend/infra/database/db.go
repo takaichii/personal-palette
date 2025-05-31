0df776b6b07037da2dceb86fc574a87123cd7725
Takazu8108180 <takaichi.0818.tech@gamil.com>
2025-05-31 17:47:05 +0900

    add: setup connect db

---
+++ backend/infra/database/db.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/pkg/errors"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DB struct {
+	db *gorm.DB
+}
+
+func NewDB() (*DB, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	dbConn := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=disable TimeZone=Asia/Tokyo",
+		os.Getenv("DB_HOST"),
+		port,
+		os.Getenv("USERNAME"),
+		os.Getenv("DATABASE"),
+		os.Getenv("USERPASS"))
+
+	db, err := gorm.Open(postgres.Open(dbConn), &gorm.Config{})
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return &DB{db: db}, nil
+}
+
+// Close DB connection．
+func (d *DB) CloseDB() error {
+	sqlDb, err := d.conn.DB()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	err = sqlDb.Close()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+}
