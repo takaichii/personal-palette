e78448c71e1039add58675e864b9dbb432143319
Takazu8108180 <takaichi.0818.tech@gamil.com>
2025-06-23 23:41:24 +0900

    refactor: presenter

--- backend/adapter/web/controller/content.go
+++ backend/adapter/web/controller/content.go
@@ -1,8 +1,13 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/takazu8108180/personal-palette/backend/adapter/web/model"
 	"github.com/takazu8108180/personal-palette/backend/adapter/web/presenter"
+	"github.com/takazu8108180/personal-palette/backend/usecase"
+	"github.com/takazu8108180/personal-palette/backend/usecase/request"
 )
 
 var _ ContentController = (*ContentControllerImpl)(nil)
@@ -12,15 +17,38 @@ type ContentController interface {
 }
 
 type ContentControllerImpl struct {
+	usecase   usecase.ContentUsecase
 	presenter presenter.ContentPresenter
 }
 
-func NewContentController(presenter presenter.ContentPresenter) ContentController {
+func NewContentController(u usecase.ContentUsecase, p presenter.ContentPresenter) ContentController {
 	return &ContentControllerImpl{
-		presenter: presenter,
+		usecase:   u,
+		presenter: p,
 	}
 }
 
 func (c *ContentControllerImpl) Create(ctx *gin.Context) {
-	c.presenter.Create(ctx)
+	var requestData model.ContentCreateRequestData
+	if err := ctx.ShouldBindJSON(&requestData); err != nil {
+		c.presenter.PresentError(ctx, http.StatusBadRequest, "Invalid request data")
+		return
+	}
+
+	usecaseInput := &request.ContentCreateInput{
+		Title:  requestData.Title,
+		Genre:  requestData.Genre,
+		Review: requestData.Review,
+		Notes:  requestData.Notes,
+		Tag:    requestData.Tag,
+		Score:  requestData.Score,
+	}
+
+	outputData, err := c.usecase.Create(ctx, usecaseInput)
+	if err != nil {
+		c.presenter.PresentError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.presenter.Create(ctx, outputData)
 }
